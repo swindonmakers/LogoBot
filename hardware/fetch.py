c7371d660d94ec8b717bea95ba582cd951ddf6e2
Damian Axford <damian@axford.me.uk>
2014-10-24 16:59:46 +0100

    "auto fetch and commit"

---
+++ hardware/fetch.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Fetch web content from master branch
+
+import sys
+import os
+import shutil
+from subprocess import call
+
+
+def fetch():
+    print("Fetch")
+    print("-----")
+    
+    # fetch a bunch of stuff
+    
+    # add anything else that's appeared
+    call(["git","add","-A"]);
+        
+    # Commit and push to origin
+    call(['git','commit','-a','-m','"auto fetch and commit"']);
+    call(['git','push','origin','gh-pages']);
+    
+    # copyall('./',target_dir, include=shutil.ignore_patterns('*.md', '*.js','*.png','*.css','*.htm','*.stl'))
+        
+if __name__ == '__main__':
+    fetch()
