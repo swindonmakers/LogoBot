1f5e2ba202fe868d86c1e969a8b61db7c251a95e
RobertCL <RobertCL@iname.com>
2015-07-23 15:20:15 +0100

    Updated webpage with support for new funtions. Wait for a response from the logobot and return it to the browser. (testing pending)

--- software/firmware/LogoBotEsp8266/LogoBotEsp8266.ino
+++ software/firmware/LogoBotEsp8266/LogoBotEsp8266.ino
@@ -37,45 +37,76 @@ a:hover, button:hover {
 }
 input {
     display:inline-block;
-    width: 40%;
+    width: 30%;
     margin-bottom:12px;
+}
+.header-fixed {
+    width:100%;
+    position:fixed;
+    top:0px;
+    left:0px;
+    background-color:#337ab7;
+    color:#fff;
+    padding: 5px;
+}
+.content {
+    padding-top: 10px;
+    text-align: center;
+}
+.button30 {
+    width: 28%;
 }
 </style>
 </head>
 <body>
-Distance: <input type="text" id="dist" value="1000" /><br />
-<a href="#" onclick="l('BK', elv('dist'));">Backward</a>
-<a href="#" onclick="l('FD', elv('dist'));">Forward</a><br/>
-<hr/>
-Angle: <input type="text" id="angle" value="90" /><br />
-<a href="#" onclick="l('LT', elv('angle'));">Left</a>
-<a href="#" onclick="l('RT', elv('angle'));">Right</a><br/>
+<div class="header-fixed">Logobot: <span id="state">ready!</span></div>
+
+<div class="content">
+
+<button onclick="l('BK', elv('dist'));">Backward</button>
+<button onclick="l('FD', elv('dist'));">Forward</button><br />
+Dist: <input type="number" id="dist" value="20" /> Ang: <input type="number" id="angle" value="90" /><br />
+<button onclick="l('LT', elv('angle'));">Left</button>
+<button onclick="l('RT', elv('angle'));">Right</button><br />
 <hr />
 
-<button onclick="l('PU', null);">Pen Up</button>
-<button onclick="l('PD', null);">Pen Down</button>
+<button onclick="l('PU');">Pen Up</button>
+<button onclick="l('PD');">Pen Down</button>
 <hr />
 
-<button onclick="l('ST', null);">Stop</button>
-<button onclick="l('BZ', 500);">Horn</button>
-
+<button class="button30" onclick="l('ST');">Stop</button>
+<button class="button30" onclick="l('BZ', 500);">Horn</button>
+<button class="button30" onclick="l('SE');">E-Stop!</button>
 <hr/>
 
-<div><span id="lastCmd"></span>:<span id="lastResp"</span></div>
+<button onclick="l('TO', elv('x'), elv('y'));">Move To</button>
+X:<input style="width: 40px" type="number" id="x" value="0"/>
+Y:<input style="width: 40px" type="number" id="y" value="0"/>
+<hr />
+
+
+<button onclick="l('WT', elv('txt'));">Write text</button>
+&nbsp;
+<input type="text" id="txt" />
+</div>
 
 <script type="text/javascript">
     function el(id) { return document.getElementById(id); }
     function elv(id) { return el(id).value; }
-    function l(cmd, dist) {
+    function l(cmd, p1, p2) {
         var xhReq = new XMLHttpRequest();
         var uri = '/cmd?action=' + cmd;
-        if (dist) { uri += '&dist=' + dist; }
+        if (p1) { uri += '&p1=' + p1; }
+        if (p2) { uri += '&p2=' + p2; }
         xhReq.open('GET', uri, true);
-        xhReq.send();
-        xhReq.onload = function () {
-          el('lastCmd').innerText = cmd + ' ' + dist;
-          el('lastResp').innerText = this.responseText;
-        }
+  try {
+          xhReq.send();
+          xhReq.onload = function () {
+            el('state').innerText = cmd + ': ' + this.responseText;
+          }
+  } catch(ex) {
+    el('state').innerText = 'send error';
+  }
     };
 </script>
 </body>
@@ -93,12 +124,28 @@ void handleCommand()
 {
   digitalWrite(led, 1);
   
-  if (server.hasArg("action") && server.hasArg("dist"))
-    Serial.println(server.arg("action") + " " + server.arg("dist"));
-  else if (server.hasArg("action"))
-    Serial.println(server.arg("action")); 
-  
-  server.send(200, F("text/plain"), "ok");
+  if (server.hasArg("action")) {
+    // Parse args, todo, url decode
+    String cmd = server.arg("action");
+    if (server.hasArg("p1")) cmd += " " + server.arg("p1");
+    if (server.hasArg("p2")) cmd += " " + server.arg("p2");
+
+    // Clear input button in anticipation of a response.
+    while(Serial.available())
+      Serial.read();
+
+    // Send command
+    Serial.println(cmd);
+
+    // Wait a max of 500ms for a repsonse
+    Serial.setTimeout(500);
+    String response = Serial.readStringUntil('\n');
+
+    server.send(200, F("text/plain"), response);
+
+  } else {
+    server.send(200, F("text/plain"), "err");
+  }
   
   digitalWrite(led, 0);
 }
