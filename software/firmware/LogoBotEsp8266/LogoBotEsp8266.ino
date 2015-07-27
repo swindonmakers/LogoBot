c9f504708c0a0361a97c4858f3b0db94e57a63af
RobertCL <RobertCL@iname.com>
2015-07-27 22:48:37 +0100

    Move webpage into separate file for clarity and to hopefully fix intermittent compile / parse bug in Arduino IDE

--- software/firmware/LogoBotEsp8266/LogoBotEsp8266.ino
+++ software/firmware/LogoBotEsp8266/LogoBotEsp8266.ino
@@ -2,6 +2,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <WiFiClient.h>
+#include "WebPages.h"
 
 /* Settings you may want to configure for your LogoBot */
 #define LOGOBOT_SSID "LogobotBlue"
@@ -17,116 +18,6 @@ static const IPAddress mask(255, 255, 255, 0);
 
 DNSServer dnsServer;
 ESP8266WebServer server(80);
-
-static const char page[] PROGMEM = R"~(
-<!DOCTYPE html>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<html>
-<head>
-<title>Logobot Blue</title>
-<style>
-body, button,input {font-family:sans-serif; font-size: 22px;}
-body {
-    padding: 5% 5%;
-}
-a, button {
-    display:inline-block;
-    width:45%;
-    background-color:#337ab7;
-    text-align:center;
-    border:1px solid #2e6da4;
-    border-radius:4px;
-    text-decoration:none;
-    color:#fff;
-    vertical-align:middle;
-    padding: 12px 0;
-}
-a+a, button+button {
-    margin-left:5%;
-}
-a:hover, button:hover {
-    background-color:#23527c;
-}
-input {
-    display:inline-block;
-    width: 30%;
-    margin-bottom:12px;
-}
-.header-fixed {
-    width:100%;
-    position:fixed;
-    top:0px;
-    left:0px;
-    background-color:#337ab7;
-    color:#fff;
-    padding: 5px;
-}
-.content {
-    padding-top: 15px;
-    text-align: center;
-}
-.button30 {
-    width: 28%;
-}
-</style>
-</head>
-<body>
-<div class="header-fixed">Logobot: <span id="state">ready!</span></div>
-
-<div class="content">
-
-<button onclick="l('BK', elv('dist'));">Backward</button>
-<button onclick="l('FD', elv('dist'));">Forward</button><br />
-Dist: <input type="number" id="dist" value="20" /> <br />
-Ang: <input type="number" id="angle" value="90" /><br />
-<button onclick="l('LT', elv('angle'));">Left</button>
-<button onclick="l('RT', elv('angle'));">Right</button><br />
-<hr />
-
-<button onclick="l('PU');">Pen Up</button>
-<button onclick="l('PD');">Pen Down</button>
-<hr />
-
-<button class="button30" onclick="l('!ST');">Stop</button>
-<button class="button30" onclick="l('BZ', 500);">Horn</button>
-<button class="button30" onclick="l('!SE');">E-Stop!</button>
-<hr/>
-
-X:<input style="width: 40px" type="number" id="x" value="0"/>
-Y:<input style="width: 40px" type="number" id="y" value="0"/>
-<br />
-<button onclick="l('TO', elv('x'), elv('y'));">Move To</button>
-<hr />
-
-
-<button onclick="l('WT', elv('txt'));">Write text</button>
-&nbsp;
-<input type="text" id="txt" />
-</div>
-
-<script type="text/javascript">
-    function el(id) { return document.getElementById(id); }
-    function elv(id) { return el(id).value; }
-    function l(cmd, p1, p2) {
-        var xhReq = new XMLHttpRequest();
-        var uri = '/cmd?action=' + cmd;
-        if (p1) { uri += '&p1=' + p1; }
-        if (p2) { uri += '&p2=' + p2; }
-        xhReq.open('GET', uri, true);
-  try {
-          xhReq.send();
-          xhReq.onload = function () {
-            el('state').innerText = this.responseText;
-          }
-  } catch(ex) {
-    el('state').innerText = 'send error';
-  }
-    };
-</script>
-</body>
-</html>
-
-)~";
 
 static void urldecode2(char *dst, const char *src)
 {
@@ -158,7 +49,7 @@ static void urldecode2(char *dst, const char *src)
 
 static void handleRoot() {
   digitalWrite(led, 1);
-  server.send(200, F("text/html"), page);
+  server.send(200, F("text/html"), mainPage);
   digitalWrite(led, 0);
 }
 
