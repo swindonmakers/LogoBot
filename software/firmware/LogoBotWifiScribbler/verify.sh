db59f6ecf280802974ec1a73f735ad0e9623529c
Damian Axford <damian@axford.me.uk>
2015-07-31 11:49:39 +0100

    Improved verify script
    Handles alternate Arduino path

--- software/firmware/LogoBotWifiScribbler/verify.sh
+++ software/firmware/LogoBotWifiScribbler/verify.sh
@@ -1,2 +1,13 @@
-
-/Applications/Arduino.app/Contents/MacOS/JavaApplicationStub --verify --board arduino:avr:pro:cpu=16MHzatmega328 --pref build.path=~/tmp  $PWD/LogoBotWifiScribbler.ino
+#!/bin/bash
+# Command line wrapper for Arduino
+P1="/Applications/Arduino.app/Contents/MacOS/JavaApplicationStub"
+P2="/Applications/Arduino 2.app/Contents/MacOS/JavaApplicationStub"
+CMD="--verify --board arduino:avr:pro:cpu=16MHzatmega328 --pref build.path=~/tmp  $PWD/LogoBotWifiScribbler.ino"
+if [ -e "$P1" ]; then
+    echo "Found: $P2"
+    "$P1" $CMD
+fi
+if [ -e "$P2" ]; then
+    echo "Found: $P2"
+    "$P2" $CMD
+fi
