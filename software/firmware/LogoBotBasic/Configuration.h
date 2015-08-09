310ab80396ca9cd12590702e9c3baeb8ab201070
Damian Axford <damian@axford.me.uk>
2015-08-09 11:21:03 +0100

    Rendered shell and start of basic

---
+++ software/firmware/LogoBotBasic/Configuration.h
@@ -0,0 +1,31 @@
+#ifndef Configuration_h
+#define Configuration_h
+
+// Pin definitions
+// Left motor driver
+#define MOTOR_L_PIN_1       2     // IN1 on the ULN2003 driver 1
+#define MOTOR_L_PIN_2       3     // IN2 on the ULN2003 driver 1
+#define MOTOR_L_PIN_3       4     // IN3 on the ULN2003 driver 1
+#define MOTOR_L_PIN_4       5     // IN4 on the ULN2003 driver 1
+
+// Right motor driver
+#define MOTOR_R_PIN_1       6     // IN1 on the ULN2003 driver 1
+#define MOTOR_R_PIN_2       7     // IN2 on the ULN2003 driver 1
+#define MOTOR_R_PIN_3       8     // IN3 on the ULN2003 driver 1
+#define MOTOR_R_PIN_4       9     // IN4 on the ULN2003 driver 1
+
+// Piezo buzzer
+#define BUZZER_PIN          10
+
+// Microswitches
+#define SWITCH_FL_PIN	    A0
+#define SWITCH_FR_PIN	    A1
+#define SWITCH_BL_PIN	    A2
+#define SWITCH_BR_PIN	    A3
+
+// IR Reflectance sensors
+#define IR_LEFT_PIN         11
+#define IR_RIGHT_PIN        12
+
+
+#endif
