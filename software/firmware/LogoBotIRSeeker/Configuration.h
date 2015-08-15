e6615395be8761d1f4f44f8e417ece84887bd89b
RobertCL <RobertCL@iname.com>
2015-08-15 21:48:07 +0100

    LogobotIR Seeker inital sketch

---
+++ software/firmware/LogoBotIRSeeker/Configuration.h
@@ -0,0 +1,35 @@
+#ifndef Configuration_h
+#define Configuration_h
+
+// Pin definitions
+// Left motor driver
+#define MOTOR_L_PIN_1       2     // IN1 on the ULN2003 driver 1
+#define MOTOR_L_PIN_2       3     // IN2 on the ULN2003 driver 1
+#define MOTOR_L_PIN_3       4     // IN3 on the ULN2003 driver 1
+#define MOTOR_L_PIN_4       5     // IN4 on the ULN2003 driver 1
+
+// Right motor driver
+#define MOTOR_R_PIN_1       6     // IN1 on the ULN2003 driver 1
+#define MOTOR_R_PIN_2       7     // IN2 on the ULN2003 driver 1
+#define MOTOR_R_PIN_3       8     // IN3 on the ULN2003 driver 1
+#define MOTOR_R_PIN_4       9     // IN4 on the ULN2003 driver 1
+
+// Piezo buzzer
+#define BUZZER_PIN          10
+
+// Microswitches
+#define SWITCH_FL_PIN	    A0
+#define SWITCH_FR_PIN	    A1
+#define SWITCH_BL_PIN	    A2
+#define SWITCH_BR_PIN	    A3
+
+// LED
+#define LED_RED_PIN         11
+#define LED_GREEN_PIN	    12
+#define LED_BLUE_PIN	    13
+
+// Lid
+#define SERVO_PIN           A4
+#define IR_PIN              A5
+
+#endif
---
+++ software/firmware/LogoBotIRSeeker/LogoBotIRSeeker.ino
@@ -0,0 +1,146 @@
+#include "Configuration.h"
+#include <DifferentialStepper.h>
+#include <Bot.h>
+#include <Servo.h>
+
+#define SCAN_SPEED 25
+int lookAng = 90; // 0 -> right, 180 -> left, 90 -> straight ahead
+int scanDir = 1;
+long lastScanMove = 0;
+
+#define DIST_CLOSE 140
+#define DIST_TOO_CLOSE 70
+
+Bot bot(MOTOR_L_PIN_1, MOTOR_L_PIN_2, MOTOR_L_PIN_3, MOTOR_L_PIN_4, MOTOR_R_PIN_1, MOTOR_R_PIN_2, MOTOR_R_PIN_3, MOTOR_R_PIN_4);
+Servo lidServo;
+
+void setup()
+{
+    initLEDs();
+    setLEDColour(0,0,1);  // Blue, as we get started
+
+    Serial.begin(9600);
+    Serial.println("Logobot");
+    bot.begin(true);
+    bot.enableLookAhead(true);
+    bot.initBumpers(SWITCH_FL_PIN, SWITCH_FR_PIN, SWITCH_BL_PIN, SWITCH_BR_PIN, handleCollision);
+
+    initBuzzer();
+    playStartupJingle();
+
+    lidServo.attach(SERVO_PIN);
+    lidServo.write(90);
+    
+    pinMode(IR_PIN, INPUT);
+}
+
+void loop()
+{
+    // keep the bot moving (this triggers the stepper motors to move, so needs to be called frequently, i.e. >1KHz)
+    bot.run();
+
+    // scan servo and check for interaction every so often
+    if (lastScanMove < millis()) {
+      float dist = readDistance();
+
+      if (dist < DIST_CLOSE) {
+        // object close, turn to face or drive towards if already facing
+        if (lookAng < 90) {
+          setLEDColour(0, 1, 0);
+          bot.turn(-1);
+          lookAng += 1;
+        } else if (lookAng > 90) {
+          setLEDColour(0, 1, 0);
+          bot.turn(1);
+          lookAng -= 1;
+        } else if (lookAng == 90 && !bot.isBusy()) {
+          if (dist < DIST_TOO_CLOSE) {
+            // too close!  back off
+            setLEDColour(1, 0, 0);
+            bot.drive(-10);
+          } else {
+            setLEDColour(0, 1, 0);
+            // head for object
+            bot.drive(10);
+          }
+        }
+      } else {
+        // keep scanning for objects
+        setLEDColour(0, 0, 1); // blue, seeking
+        lookAng += scanDir;
+        if (lookAng > 180) scanDir = -1;
+        if (lookAng < 0) scanDir = 1;
+      }
+      lidServo.write(lookAng);
+      lastScanMove = millis() + SCAN_SPEED;
+    }
+}
+
+// returns distance in mm
+static float readDistance()
+{
+  return 123438.5 * pow(analogRead(IR_PIN), -1.15);
+}
+
+static void handleCollision(byte collisionData)
+{
+    if (collisionData != 0) {
+        // Just hit something, so stop, buzz and backoff
+        setLEDColour(1,0,0);  // Red, because we hit something
+        Serial.println("Ouch!");
+        bot.emergencyStop();
+        bot.buzz(500);
+        bot.drive(-20);
+        playGrumpy();
+    }
+
+    // Insert some recovery based on which bumper was hit
+    // Queue the moves directly to bot, don't bother with Logo command queue
+    if (collisionData == 1) {
+        lookAng=135;
+        bot.turn(-30);
+    } else if (collisionData == 2) {
+        lookAng=45;
+        bot.turn(60);
+    } else if (collisionData != 0) {
+        bot.turn(-90);
+    }
+}
+
+static void initLEDs() {
+    // LED setup
+    pinMode(LED_RED_PIN, OUTPUT);
+    pinMode(LED_GREEN_PIN, OUTPUT);
+    pinMode(LED_BLUE_PIN, OUTPUT);
+    setLEDColour(0,0,0);
+}
+
+static void setLEDColour(byte r, byte g, byte b) {
+    digitalWrite(LED_RED_PIN, r);
+    digitalWrite(LED_GREEN_PIN, g);
+    digitalWrite(LED_BLUE_PIN, b);
+}
+
+static void initBuzzer() {
+    pinMode(BUZZER_PIN, OUTPUT);
+}
+
+static void playStartupJingle() {
+    for (byte i=0; i<3; i++) {
+        analogWrite(BUZZER_PIN, 100 + i*50);
+        delay(200 + i*50);
+    }
+
+    analogWrite(BUZZER_PIN, 0);
+}
+
+static void playGrumpy() {
+    for (byte i=0; i<2; i++) {
+        analogWrite(BUZZER_PIN, 250 - i*100);
+        delay(100 + i*100);
+    }
+
+    analogWrite(BUZZER_PIN, 0);
+}
+
+
