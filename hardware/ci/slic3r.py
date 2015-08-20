183d476bfe2b2a94fd2d9076eb142a18eb046c42
Axford <damian@axford.me.uk>
2015-08-20 11:59:40 +0100

    Fix for no slic3r install

--- hardware/ci/slic3r.py
+++ hardware/ci/slic3r.py
@@ -9,22 +9,25 @@ import os
 
 
 def calc_plastic_required(stlpath):
-    p = re.compile("\((.*)cm3\)", re.MULTILINE)
-    o = check_output(['slic3r','-o','tmp.gcode',stlpath])
-    res = p.search(o)
+    try:
+        p = re.compile("\((.*)cm3\)", re.MULTILINE)
+        o = check_output(['slic3r','-o','tmp.gcode',stlpath])
+        res = p.search(o)
 
-    #remove tmp.gcode
-    if os.path.isfile('tmp.gcode'):
-        os.remove('tmp.gcode')
+        #remove tmp.gcode
+        if os.path.isfile('tmp.gcode'):
+            os.remove('tmp.gcode')
 
-    # Match stdout of form: Filament required: 9439.3mm (66.7cm3)
-    # weight based on density of 1.26 g/cm3
+        # Match stdout of form: Filament required: 9439.3mm (66.7cm3)
+        # weight based on density of 1.26 g/cm3
 
-    if res:
-        vol = float(res.group(1))
-        weight = vol * 1.25/1000
-        return {'plasticVolume': vol, 'plasticWeight':weight}
-    else:
+        if res:
+            vol = float(res.group(1))
+            weight = vol * 1.25/1000
+            return {'plasticVolume': vol, 'plasticWeight':weight}
+        else:
+            return {}
+    except:
         return {}
 
 if __name__ == '__main__':
