23f582f7fb83ce5f7bf950f0f41bbfbce45da3da
Damian Axford <damian@axford.me.uk>
2015-08-23 15:35:40 +0100

    Added automatic heading ids to index
    To support anchor links

--- hardware/docs/js/index.js
+++ hardware/docs/js/index.js
@@ -1,3 +1,12 @@
-// super simple module for the most common nodejs use case.
-exports.markdown = require("./markdown");
-exports.parse = exports.markdown.toHTML;
+$(document).ready(function(){
+    // add ids to headings to accommodate anchor links
+    $('h1').each(function(index) {
+      $(this).attr('id', $(this).text().replace(/[\s]+/g, "-").replace(/[\.]+/g,"").toLowerCase());
+    });
+    $('h2').each(function(index) {
+      $(this).attr('id', $(this).text().replace(/[\s]+/g, "-").replace(/[\.]+/g,"").toLowerCase());
+    });
+    $('h3').each(function(index) {
+      $(this).attr('id', $(this).text().replace(/[\s]+/g, "-").replace(/[\.]+/g,"").toLowerCase());
+    });
+});
