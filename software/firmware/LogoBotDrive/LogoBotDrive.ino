c8573237219717019bd16b1b463c072a42e2149c
Jess Robinson <castaway@desert-island.me.uk>
2018-05-06 18:36:21 +0100

    Initial rfid reader bot code (untested)

---
+++ software/firmware/LogoBotDrive/LogoBotDrive.ino
@@ -0,0 +1,121 @@
+#include "Configuration.h"
+#include <DifferentialStepper.h>
+#include <Bot.h>
+#include <Servo.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_PN532.h>
+
+Bot bot(MOTOR_L_PIN_1, MOTOR_L_PIN_2, MOTOR_L_PIN_3, MOTOR_L_PIN_4, MOTOR_R_PIN_1, MOTOR_R_PIN_2, MOTOR_R_PIN_3, MOTOR_R_PIN_4);
+String cmd; // cmd received over serial - builds up char at a time
+Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
+
+void setup()
+{
+    //initLEDs();
+    //setLEDColour(0,0,1);  // Blue, as we get started
+
+    Serial.begin(9600);
+    Serial.println("Logobot");
+    bot.begin();
+    nfc.begin();
+    nfc.SAMConfig();
+    bot.enableLookAhead(true);
+    bot.initBumpers(SWITCH_FL_PIN, SWITCH_FR_PIN, SWITCH_BL_PIN, SWITCH_BR_PIN, handleCollision);
+
+    //initBuzzer();
+    //playStartupJingle();
+}
+
+void loop()
+{
+    // keep the bot moving (this triggers the stepper motors to move, so needs to be called frequently, i.e. >1KHz)
+    bot.run();
+
+    uint8_t direction = readNFC();
+    if (!bot.isQFull()) {  // fill up the move queue to get the speed up
+        //setLEDColour(0,1,0); // Green, because we are happily doing something
+        // keep it moving, until it hits something
+        bot.drive(10);
+    }
+}
+
+uint8_t readNFC() {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+    
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  
+  if (success) {
+    // Display some basic information about the card
+    //Serial.println("Found an ISO14443A card");
+    //Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print("  UID Value: ");
+    nfc.PrintHex(uid, uidLength);
+    Serial.println("");   
+  }
+  return uid;
+}
+
+static void handleCollision(byte collisionData)
+{
+    if (collisionData != 0) {
+        // Just hit something, so stop, buzz and backoff
+        setLEDColour(1,0,0);  // Red, because we hit something
+        Serial.println("Ouch!");
+        bot.emergencyStop();
+        bot.buzz(500);
+        bot.drive(-20);
+        playGrumpy();
+    }
+
+    // Insert some recovery based on which bumper was hit
+    // Queue the moves directly to bot, don't bother with Logo command queue
+    if (collisionData == 1) {
+        bot.turn(-30);
+    } else if (collisionData == 2) {
+        bot.turn(60);
+    } else if (collisionData != 0) {
+        bot.turn(-90);
+    }
+}
+
+static void initLEDs() {
+    // LED setup
+    pinMode(LED_RED_PIN, OUTPUT);
+    pinMode(LED_GREEN_PIN, OUTPUT);
+    pinMode(LED_BLUE_PIN, OUTPUT);
+    setLEDColour(0,0,0);
+}
+
+static void setLEDColour(byte r, byte g, byte b) {
+    digitalWrite(LED_RED_PIN, r);
+    digitalWrite(LED_GREEN_PIN, g);
+    digitalWrite(LED_BLUE_PIN, b);
+}
+
+static void initBuzzer() {
+    pinMode(BUZZER_PIN, OUTPUT);
+}
+
+static void playStartupJingle() {
+    for (byte i=0; i<3; i++) {
+        analogWrite(BUZZER_PIN, 100 + i*50);
+        delay(200 + i*50);
+    }
+
+    analogWrite(BUZZER_PIN, 0);
+}
+
+static void playGrumpy() {
+    for (byte i=0; i<2; i++) {
+        analogWrite(BUZZER_PIN, 250 - i*100);
+        delay(100 + i*100);
+    }
+
+    analogWrite(BUZZER_PIN, 0);
+}
---
+++ software/firmware/LogoBotDrive/upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Command line wrapper for Arduino
+P1="/Applications/Arduino.app/Contents/MacOS/JavaApplicationStub"
+P2="/Applications/Arduino 2.app/Contents/MacOS/JavaApplicationStub"
+CMD="--upload --board arduino:avr:pro:cpu=8MHzatmega328 --pref build.path=~/tmp  $PWD/*.ino"
+if [ -e "$P1" ]; then
+    echo "Found: $P2"
+    "$P1" $CMD
+fi
+if [ -e "$P2" ]; then
+    echo "Found: $P2"
+    "$P2" $CMD
+fi
+screen /dev/tty.usbserial-A800JS6J
